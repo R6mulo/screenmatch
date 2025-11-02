4fca52e3be60715b185936f41bbbe55044c3e328
R6mulo <r6rorschach.dev@gmail.com>
2025-11-02 14:09:22 -0300

    Aulas 4 e 5

---
+++ src/br/com/curso/screenmatch/modelos/Título.java
@@ -0,0 +1,71 @@
+package br.com.curso.screenmatch.modelos;
+
+public class Título implements Comparable<Título> {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public Título(String nome, int anoDeLancamento) {
+        this.nome = nome;
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalAvaliacoes(){
+        return totalAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do Filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalAvaliacoes;
+    }
+
+    @Override
+    public int compareTo(Título outroTítulo) {
+        return this.getNome().compareTo(outroTítulo.getNome());
+    }
+
+}
